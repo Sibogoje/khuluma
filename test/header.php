128998538a3ed040aa8b51df6a89bc0362c55303
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-20 05:37:21 +0200

    update navigation links and contact number in header.php

--- test/header.php
+++ test/header.php
@@ -14,23 +14,23 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto mr-2">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="Index.php">Home <span class="sr-only">(current)</span></a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="about.html">About </a>
+                <a class="nav-link" href="About Us.php">About Us </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="portfolio.html">Portfolio </a>
+                <a class="nav-link" href="why your story matters.php">Why your story matters </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="service.html">Services</a>
+                <a class="nav-link" href="Service.php">Services</a>
               </li>
 
               <li class="nav-item">
-                <a class="nav-link" href="contact.html">Contact us</a>
+                <a class="nav-link" href="Contact Us.php">Contact Us</a>
               </li>
             </ul>
-            <div class="user_option">
+            <!-- <div class="user_option">
               <div class="login_btn-container">
                 <a href="">
                   Login
@@ -39,11 +39,11 @@
               <form class="form-inline my-2 my-lg-0">
                 <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
               </form>
-            </div>
+            </div> -->
           </div>
           <div class="call_btn">
             <a href="">
-              Call: +01234567890
+              Call: +268 76285502
             </a>
           </div>
         </nav>
