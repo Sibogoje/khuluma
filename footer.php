cea76ef3a2b5db86d65c4492a091d839012fd190
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-20 17:06:29 +0200

    fix: restructure footer layout for improved organization and readability

--- footer.php
+++ footer.php
@@ -1,102 +1,102 @@
 <section class="info_section layout_padding-top layout_padding2-bottom">
     <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-lg-4">
-          <div class="info_links pl-lg-5">
-            <h4>
-              Menu
-            </h4>
-            <ul>
-              <li>
-                <a href="index.php">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="About Us.php">
-                  About
-                </a>
-              </li>
-              <li>
-                <a class="" href="why your story matters.php">Brand Stories </a>
-              </li>
-              <li class="">
-                <a class="" href="Our Services.php">Services</a>
-              </li>
-              <li>
-                <a href="Contact Us.php">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="info_links pl-lg-5">
+                    <h4>
+                        Menu
+                    </h4>
+                    <ul>
+                        <li>
+                            <a href="index.php">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="About Us.php">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a class="" href="why your story matters.php">Brand Stories </a>
+                        </li>
+                        <li class="">
+                            <a class="" href="Our Services.php">Services</a>
+                        </li>
+                        <li>
+                            <a href="Contact Us.php">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="info_contact">
+                    <h4>
+                        Location
+                    </h4>
+                    <div>
+                        <img src="images/location.png" alt="" />
+                        <p style="color: white;">
+                            SNAT COOPS Building<br>
+                            Office # 314 <br>
+                            Mancishane Street, Manzini
+                        </p>
+                    </div>
+                    <div>
+                        <img src="images/telephone.png" alt="" />
+                        <p style="color: white;">
+                            (+268) 7628 5502 <br> (+268) 7913 8585
+                        </p>
+                    </div>
+                    <div>
+                        <img src="images/envelope.png" alt="" />
+                        <p style="color: white;">
+                            communications@khulumaeswatini.com
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="info_social">
+                    <h4>
+                        Social Media Links
+                    </h4>
+                    <div class="social_container">
+                        <div>
+                            <a href="https://www.facebook.com/profile.php?id=61572627470346" target="_blank">
+                                <img src="images/facebook-logo.png" alt="" />
+                            </a>
+                        </div>
+                        <!-- <div>
+                            <a href="">
+                                <img src="images/twitter-logo.png" alt="" />
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <img src="images/instagram.png" alt="" />
+                            </a>
+                        </div> -->
+                        <div>
+                            <a href="https://www.linkedin.com/company/khuluma-eswatini/posts/?feedView=all" target="_blank">
+                                <img src="images/linkedin-sign.png" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="info_contact">
-            <h4>
-              Location
-            </h4>
-            <div>
-              <img src="images/location.png" alt="" />
-              <p style="color: white;">
-                SNAT COOPS Building<br>
-                Office # 314 <br>
-                Mancishane Street, Manzini
-              </p>
-            </div>
-            <div>
-              <img src="images/telephone.png" alt="" />
-              <p style="color: white;">
-                (+268) 7628 5502 <br> (+268) 7913 8585
-              </p>
-            </div>
-            <div>
-              <img src="images/envelope.png" alt="" />
-              <p style="color: white;">
-                communications@khulumaeswatini.com
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-4">
-          <div class="info_social">
-            <h4>
-              Social Media Links
-            </h4>
-            <div class="social_container">
-              <div>
-                <a href="https://www.facebook.com/profile.php?id=61572627470346" target="_blank">
-                  <img src="images/facebook-logo.png" alt="" />
-                </a>
-              </div>
-              <!-- <div>
-                <a href="">
-                  <img src="images/twitter-logo.png" alt="" />
-                </a>
-              </div>
-              <div>
-                <a href="">
-                  <img src="images/instagram.png" alt="" />
-                </a>
-              </div> -->
-              <div>
-                <a href="https://www.linkedin.com/company/khuluma-eswatini/posts/?feedView=all" target="_blank">
-                  <img src="images/linkedin-sign.png" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </section>
+</section>
 
-  <!-- end info_section -->
+<!-- end info_section -->
 
-  <!-- footer section -->
-  <footer class="container-fluid footer_section">
+<!-- footer section -->
+<footer class="container-fluid footer_section">
     <p>
-      &copy; 2025 All Rights Reserved By
-      <a href="https://khulumaeswatini.com/">Khuluma Eswatini</a>
+        &copy; 2025 All Rights Reserved By
+        <a href="https://khulumaeswatini.com/">Khuluma Eswatini</a>
     </p>
-  </footer>
+</footer>
