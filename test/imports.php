4eddc6d81454d94cf340f48e3957fdc7d3221e42
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-20 06:08:28 +0200

    fix: update version in style.css link from 1.0 to 1.1 for cache busting

--- test/imports.php
+++ test/imports.php
@@ -8,6 +8,6 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
 
   <!-- Custom styles for this template -->
-  <link href="css/style.css?v=1.0" rel="stylesheet" />
+  <link href="css/style.css?v=1.1" rel="stylesheet" />
   <!-- responsive style -->
   <link href="css/responsive.css" rel="stylesheet" />
