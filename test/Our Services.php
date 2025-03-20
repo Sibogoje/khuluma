8be5e9a901bb47f04a9da49625282903f14c674d
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-20 13:00:55 +0200

    refactor: update services section with new content and structure

--- test/Our Services.php
+++ test/Our Services.php
@@ -35,136 +35,57 @@
 
   <!-- custom menu -->
 
-  <!-- do section -->
-  <section class="do_section layout_padding">
+  <!-- services section -->
+  <section class="services_section layout_padding">
     <div class="container">
       <div class="custom_heading-container">
-        <h2>
-          What we do
-        </h2>
+        <h2>Our Services</h2>
       </div>
       <div class="row">
-        <div class="col-md-3 col-sm-6">
-          <div class="content-box bg-red">
-            <div class="img-box">
-              <img src="images/idea.png" alt="" />
-            </div>
-            <div class="detail-box">
-              <h6>
-                Original Ideas
-              </h6>
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content
-              </p>
-            </div>
+        <div class="col-md-6">
+          <div class="service-box">
+            <h3>Marketing Communication</h3>
+            <ul>
+              <li>Social Media Management</li>
+              <li>Content Creation (Blogs, Graphics, Videos)</li>
+              <li>Campaign Development</li>
+            </ul>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="content-box bg-green">
-            <div class="img-box">
-              <img src="images/controller.png" alt="" />
-            </div>
-            <div class="detail-box">
-              <h6>
-                Great apps
-              </h6>
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content
-              </p>
-            </div>
+        <div class="col-md-6">
+          <div class="service-box">
+            <h3>Brand and Media Relations</h3>
+            <ul>
+              <li>Brand Identity and Logo Development</li>
+              <li>Reputation and Crisis Management</li>
+              <li>Press Releases</li>
+            </ul>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="content-box bg-red">
-            <div class="img-box">
-              <img src="images/monitor.png" alt="" />
-            </div>
-            <div class="detail-box">
-              <h6>
-                High Resolution
-              </h6>
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content
-              </p>
-            </div>
+        <div class="col-md-6">
+          <div class="service-box">
+            <h3>Editorial Services and Publications</h3>
+            <ul>
+              <li>Annual Reports and Internal Communications</li>
+              <li>Commemorative Publications and Reports</li>
+              <li>Editing of academic research</li>
+            </ul>
           </div>
         </div>
-        <div class="col-md-3 col-sm-6">
-          <div class="content-box bg-green">
-            <div class="img-box">
-              <img src="images/rocket-ship.png" alt="" />
-            </div>
-            <div class="detail-box">
-              <h6>
-                Fast Loading
-              </h6>
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content
-              </p>
-            </div>
+        <div class="col-md-6">
+          <div class="service-box">
+            <h3>Training Services</h3>
+            <ul>
+              <li>Public Relations and Media Relations Training</li>
+              <li>Interview Preparation and Career Coaching</li>
+              <li>Theological Studies and Communication</li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
   </section>
-
-  <!-- end do section -->
-  <!-- skill section -->
-
-  <section class="skill_section layout_padding2">
-    <div class="container">
-      <div class="custom_heading-container">
-        <h2>
-          Our Skills
-        </h2>
-      </div>
-      <div class="skill_padding">
-        <div class="row">
-          <div class="col-md-3 col-sm-6">
-            <div class="box">
-              <div class="circle" id="circles-1"></div>
-              <h6>
-                Adobe Photoshop
-              </h6>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <div class="box">
-              <div class="circle" id="circles-2"></div>
-              <h6>
-                Adobe Ilustrator
-              </h6>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <div class="box">
-              <div class="circle" id="circles-3"></div>
-              <h6>
-                After Effects
-              </h6>
-            </div>
-          </div>
-          <div class="col-md-3 col-sm-6">
-            <div class="box">
-              <div class="circle" id="circles-4"></div>
-              <h6>
-                Adobe XD
-              </h6>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- end skill section -->
-
-
-
+  <!-- end services section -->
 
   <!-- info section -->
 <?php include "footer.php"; ?>
