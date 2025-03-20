a29f7db52cf85b6357b58da427bc82b7a8a94151
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-20 17:39:34 +0200

    fix: set max-width for graph image to enhance responsiveness

--- index.php
+++ index.php
@@ -171,7 +171,7 @@
             </div>
                 <div class="col-md-7 col-lg-8">
                   <div class="img-box" >
-                    <img src="images/graph.png" alt="" />
+                    <img src="images/graph.png" alt="" style="max-width: 50%;" />
                   </div>
                 </div>
               </div>
