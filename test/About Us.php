64246d64fcf50f9414435ef1446d39dec169d016
Siboniso <sibandzesiboniso0@gmail.com>
2025-03-20 06:08:15 +0200

    refactor: move stylesheet links to imports.php for better organization

--- test/About Us.php
+++ test/About Us.php
@@ -12,18 +12,8 @@
   <meta name="description" content="" />
   <meta name="author" content="" />
 
-  <title>Brainwave</title>
+  <title>About Us</title>
 
-  <!-- slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
   <?php include "imports.php"; ?>
 </head>
 
--- test/Contact Us.php
+++ test/Contact Us.php
@@ -13,17 +13,6 @@
   <meta name="author" content="" />
 
   <title>Contact Us</title>
-
-  <!-- slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Poppins:400,700|Roboto:400,700&display=swap" rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
   <?php include "imports.php"; ?>
 </head>
 
